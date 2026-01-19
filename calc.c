187c5cab018b676d54783f2c39a9755676c68bb6
unknown <lokeshabasavasiri1140>
2026-01-19 09:11:47 +0530

    Add a new file

---
+++ calc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main
+{
+int a=5;
+int b=5;
+int add=a+b;
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
+printf("add: %d", add);
+printf("subtraction: %d", sub);
+printf("multiplication: %d", mul);
+printf("division: %d", div);
+}
